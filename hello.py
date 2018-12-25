93b96a1f2f6d3b3ff91d8d75d5208e9762b07219
agoponenko <andreigoponenko@gmail.com>
2018-12-25 15:08:52 +0300

    worked with flask get post

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, redirect, url_for, render_template, send_file, flash
 import numpy as np
 from sklearn.externals import joblib 
+import pandas as pd
 
 app = Flask(__name__)
 knn = joblib.load('knn.jbl')
@@ -40,12 +41,97 @@ def show_image():
     return  '<img src="/static/setosa.jpg" alt="setosa">' # works only on /static wtf?
 
 
+@app.route('/badrequest400')
+def bad_request():
+    return abort(400)
+
 @app.route('/iris_post', methods=['GET', 'POST'])
 def add_message():
-    content = request.get_json()
-    params = content['flower'].split(',')
-    params = [float(param) for param in params]
-    params = np.array(params).reshape(1,-1)
-    predictions = knn.predict(params)
-    predictions = {'class':str(predictions[0])}
+    try:
+        content = request.get_json()
+        params = content['flower'].split(',')
+        params = [float(param) for param in params]
+        params = np.array(params).reshape(1,-1)
+        predictions = knn.predict(params)
+        predictions = {'class':str(predictions[0])}
+    except:
+        return redirect(url_for('bad_request'))
     return jsonify(predictions)
+
+#Creating form to upload file to score
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField
+from wtforms.validators import DataRequired
+from werkzeug.utils import secure_filename
+import os
+
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    file = FileField()
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+
+        f = form.file.data
+        filename = form.name.data + '.csv'
+        # f.save(os.path.join(
+        #     filename
+        # ))
+        df = pd.read_csv(f, header=None)
+        print(df.head())
+
+        predictions = knn.predict(df)
+        result = pd.DataFrame(predictions)
+        result.to_csv(filename, index=False)
+
+        return send_file(filename,
+                    mimetype='text/csv',
+                    attachment_filename=filename,
+                    as_attachment=True)
+
+    return render_template('submit.html', form=form)
+
+
+# Uploading files kinda automatically
+UPLOAD_FOLDER = ''
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename + 'uploaded')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'file uploaded'
+            
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 docker exec -it flasltest_flask_1 bash
 
 docker exec -it flasltest_flask_1 python train_model.py
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"flower":"1,2,3,7"}' \
+  http://localhost:5000/iris_post
